18249110d4fddefc777ec84c3e74ce7826e1d092
Mazur Roman <superioritywork@gmail.com>
2022-11-20 00:31:45 +0200

    action transition

--- css/modal.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
